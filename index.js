71d64c1b54d0d44e05b974376beff365d7069f30
Alex Jordan <alex@strugee.net>
2017-04-24 13:38:15 -0400

    Use Buffer.from when available
    `new Buffer()` doesn't work on Node 6+, so we use the new stuff when possible and fall back otherwise.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = toBuffer
 
+function convert (buf, enc) {
+  return Buffer.from ? Buffer.from(buf, enc) : new Buffer(buf, enc)
+}
+
 function toBuffer (buf, enc) {
   if (Buffer.isBuffer(buf)) return buf
-  if (typeof buf === 'string') return new Buffer(buf, enc)
-  if (Array.isArray(buf)) return new Buffer(buf)
+  if (typeof buf === 'string') return convert(buf, enc)
+  if (Array.isArray(buf)) return convert(buf)
   throw new Error('Input should be a buffer or a string')
 }
