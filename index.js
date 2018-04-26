eebe20e0603e2c6a542b00316f1661741fdf1124
Mathias Buus <mathiasbuus@gmail.com>
2018-04-26 16:56:08 +0200

    use Buffer.from when avail

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 module.exports = toBuffer
 
+var makeBuffer = Buffer.from && Buffer.from !== Uint8Array.from ? Buffer.from : bufferFrom
+
+function bufferFrom (buf, enc) {
+  return new Buffer(buf, enc)
+}
+
 function toBuffer (buf, enc) {
   if (Buffer.isBuffer(buf)) return buf
-  if (typeof buf === 'string') return new Buffer(buf, enc)
-  if (Array.isArray(buf)) return new Buffer(buf)
+  if (typeof buf === 'string') return makeBuffer(buf, enc)
+  if (Array.isArray(buf)) return makeBuffer(buf)
   throw new Error('Input should be a buffer or a string')
 }
